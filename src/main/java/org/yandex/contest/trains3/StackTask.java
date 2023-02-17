50c3807528b0992bb56e66552e19731f835d10cc
willhelmatiq <rot3ad@gmail.com>
2023-02-17 18:15:12 +0100

    solve StackTask from trains 3.0

---
+++ src/main/java/org/yandex/contest/trains3/StackTask.java
@@ -0,0 +1,49 @@
+package org.yandex.contest.trains3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StackTask {
+    public static void main(String[] args) {
+        workWithStack();
+    }
+
+    static void workWithStack() {
+        Stack<Integer> stack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String[] command = scanner.nextLine().split(" ");
+            if (command.length == 2) {
+                stack.push(Integer.valueOf(command[1]));
+                System.out.println("ok");
+            } else {
+                switch (command[0]){
+                    case "pop":
+                        if (stack.empty()) {
+                            System.out.println("error");
+                        } else {
+                            System.out.println(stack.pop());
+                        }
+                        break;
+                    case "back":
+                        if (stack.empty()) {
+                            System.out.println("error");
+                        } else {
+                            System.out.println(stack.peek());
+                        }
+                        break;
+                    case "size":
+                        System.out.println(stack.size());
+                        break;
+                    case "clear":
+                        stack.clear();
+                        System.out.println("ok");
+                        break;
+                    case "exit":
+                        System.out.println("bye");
+                        System.exit(0);
+                }
+            }
+        }
+    }
+}
