c24abe470d369dc09b8430d13287fab50b0fa0e8
willhelmatiq <rot3ad@gmail.com>
2023-02-17 17:45:55 +0100

    solve bracket sequence from trains 3.0

---
+++ src/main/java/org/yandex/contest/trains3/CorrectBracketsTask.java
@@ -0,0 +1,41 @@
+package org.yandex.contest.trains3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CorrectBracketsTask {
+
+    public static void main(String[] args) {
+        if (isBracketSequenceCorrect()) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+
+    static boolean isBracketSequenceCorrect() {
+        Scanner scanner = new Scanner(System.in);
+        String brackets = scanner.nextLine();
+        scanner.close();
+        Stack<Character> bracketStack = new Stack<>();
+        for (int i = 0; i < brackets.length(); i++) {
+            char element = brackets.charAt(i);
+            if (bracketStack.empty()) {
+                if (element == ')' || element == ']' || element == '}') {
+                    return false;
+                } else {
+                    bracketStack.push(element);
+                }
+            } else {
+                if ((element == ')' && bracketStack.peek() == '(') || (element == ']' && bracketStack.peek() == '[')
+                        || (element == '}' && bracketStack.peek() == '{')) {
+                    bracketStack.pop();
+                } else {
+                    bracketStack.push(element);
+                }
+            }
+        }
+
+        return bracketStack.empty();
+    }
+}
